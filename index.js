f36d47353332f65b55aa105a0306e50496109f5e
Gia-Phong <gjnguyen@wisc.edu>
2024-05-10 10:42:46 -0500

    processed resdb data

--- index.js
+++ index.js
@@ -9,32 +9,43 @@ import { createTimeStr } from './js/date_mod.js';
 import { sendData } from './js/endpoint.js';
 import axios from 'axios';
 
-// const app = express();
-// const PORT = 8080;
-// const eventEmitter = new EventEmitter();
+const app = express();
+const PORT = 8080;
+const eventEmitter = new EventEmitter();
 
-// const DATA_DIR = "processed_data"
+const DATA_DIR = "processed_data"
 
-// eventEmitter.on('testEvent', () => {
-//     console.log("hello world2")
+eventEmitter.on('testEvent', () => {
+    console.log("hello world2")
 
-//     fs.readFile('./testFile.txt', 'utf8', (err, txt) => {
-//         console.log(txt)
-//     })
-// })
+    fs.readFile('./testFile.txt', 'utf8', (err, txt) => {
+        console.log(txt)
+    })
+})
 
-// eventEmitter.emit("testEvent")
+eventEmitter.emit("testEvent")
 
-// app.use(cors());
+app.use(cors());
 
-// app.get('/testGet', (req, res) => {
-//     sendData(res, req, DATA_DIR)
-// })
+app.get('/testGet', (req, res) => {
+    // sendData(res, req, DATA_DIR)
+    fs.readFile("./resDB_processed_data.json", 'utf8', (err, txt) => {
+        if(err) {
+            console.log("can't find file", file)
+        }
+        else {
+            let data = JSON.parse(txt)
+            res.status(200).send({
+                data: data
+            })  
+        }
+    })
+})
 
-// app.listen(
-//     PORT,
-//     () => console.log(`live on localhost:${PORT}`)
-// )
+app.listen(
+    PORT,
+    () => console.log(`live on localhost:${PORT}`)
+)
 
 // async function getData() {
 //     let endTime = new Date()
@@ -106,6 +117,56 @@ function deleteBetweenTwoPhrases(str, phrase1, phrase2, newPhrase) {
     return newstr
 }
 
+function processData(txt, outfile) {
+    let data = JSON.parse(txt);
+    let numTrans = 0;
+    let addresses = [];
+    let transactions = [];
+    for(let i = 0; i < data.length; i++) {
+        let obj = data[i]
+        if(obj.inputs) {
+            if(obj.inputs[0].owners_before) {
+                if(obj.outputs) {
+                    if(obj.outputs[0].public_keys) {
+                        if(obj.outputs[0].amount) {
+                            if(obj.asset) {
+                                if(obj.asset.data) {
+                                    if(obj.asset.data.time) {
+                                        let processedTrans = {
+                                            from: obj.inputs[0].owners_before[0],
+                                            to: obj.outputs[0].public_keys[0],
+                                            amount: obj.outputs[0].amount,
+                                            timestamp: obj.asset.data.time,
+                                            id: obj.id
+                                        }
+
+                                        transactions.push(processedTrans)
+                                        if(!addresses.includes(processedTrans.to)) {
+                                            addresses.push(processedTrans.to)
+                                        }
+                                        if(!addresses.includes(processedTrans.from)) {
+                                            addresses.push(processedTrans.from)
+                                        }
+
+                                        numTrans += 1;
+                                        // console.log(processedTrans)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    let obj = {
+        users: addresses,
+        transactions: transactions
+    }
+    saveJSON(obj, outfile)
+    // console.log(numTrans)
+}
+
 function getResDBData() {
     axios.get('https://crow.resilientdb.com/v1/transactions')
     .then((response) => {
@@ -137,51 +198,21 @@ function getResDBData() {
           }
       }
   
-      fs.writeFile("./resDB_data5.json", datastr, 'utf8', () => {
-          console.log("./resDB_data5.json", "file saved")
-      })
+      processData(datastr);
+    //   fs.writeFile("./resDB_data5.json", datastr, 'utf8', () => {
+    //       console.log("./resDB_data5.json", "file saved")
+    //   })
     });
 }
 
 // getResDBData()
-
-function processData(txt) {
-    let data = JSON.parse(txt);
-    let numTrans = 0;
-    for(let i = 0; i < data.length; i++) {
-        let obj = data[i]
-        if(obj.inputs) {
-            if(obj.inputs[0].owners_before) {
-                if(obj.outputs) {
-                    if(obj.outputs[0].public_keys) {
-                        if(obj.outputs[0].amount) {
-                            if(obj.asset) {
-                                if(obj.asset.data) {
-                                    if(obj.asset.data.time) {
-                                        let from = obj.inputs[0].owners_before[0]
-                                        let to = obj.outputs[0].public_keys[0]
-                                        let amount = obj.outputs[0].amount
-                                        let time = obj.asset.data.time
-                                        numTrans += 1;
-                                        console.log(from, to, amount, time)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    console.log(numTrans)
-}
 
 fs.readFile("./resDB_data5.json" , 'utf8', (err, txt) => {
     if(err) {
         console.log("can't find file", file)
     }
     else {
-        processData(txt);
+        processData(txt, "./resDB_processed_data.json");
     }
 })
 
