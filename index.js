a592c303ac7fc754cdc1816a1da49258d3a3c377
Gia-Phong Nguyen <giatheawesome2@gmail.com>
2024-05-28 22:09:09 -0700

    relative path for eth

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { getTimeFromStr } from './js/date_mod.js';
 import { sendData_ETH, sendData_RESDB } from './js/endpoint.js';
 import { downloadData_RESDB } from './js/data_download_resdb.js';
 
-const DATA_DIR = "processed_data";
+const DATA_DIR = "processed_data\\";
 const RESDB_OUTFILE = 'resDB_data.json';
 const ETH_QUERY_SIZE = 1000;
 
@@ -53,14 +53,14 @@ function deleteOldFiles(keep, dir) {
 // seconds - minute - hour ...
 
 // get eth data every 30 min and only keep newest 48 files (1 day old)
-schedule.scheduleJob('0 */30 * * * *', () => {
-    console.log("Downloading ETH data")
-    let endTime = new Date()
-    let MS_PER_MINUTE = 60000;
-    let startTime = new Date(endTime - 30 * MS_PER_MINUTE)
-    downloadData_ETH(ETH_QUERY_SIZE, startTime, endTime);
-    deleteOldFiles(48, './processed_data/')
-})
+// schedule.scheduleJob('0 */30 * * * *', () => {
+//     console.log("Downloading ETH data")
+//     let endTime = new Date()
+//     let MS_PER_MINUTE = 60000;
+//     let startTime = new Date(endTime - 30 * MS_PER_MINUTE)
+//     downloadData_ETH(ETH_QUERY_SIZE, startTime, endTime);
+//     deleteOldFiles(48, './processed_data/')
+// })
 
 // get resdb data every hour
 schedule.scheduleJob('0 0 */1 * * *', () => {
